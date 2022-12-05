3be3f88d9f3305f0f90e3dfc64a620a35d5b55b1
David F <davidfers@protonmail.com>
2022-12-05 18:11:24 +0100

    setup test environment

---
+++ src/components/Checkout.test.tsx
@@ -0,0 +1,7 @@
+import Checkout from './Checkout';
+
+describe('Checkout', () => {
+  test('Testing', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()]
-})
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
