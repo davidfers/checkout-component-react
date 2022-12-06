f62b016380a2efce5a10dc4f0bbd7937a9168800
David F <davidfers@protonmail.com>
2022-12-06 12:28:21 +0100

    test onsubmit when all fields are valid, test error messages, add aria-error message

--- src/components/Checkout.test.tsx
+++ src/components/Checkout.test.tsx
@@ -1,7 +1,67 @@
+import { screen, render, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { vi } from 'vitest';
 import Checkout from './Checkout';
 
 describe('Checkout', () => {
-  test('Testing', () => {
-    expect(1 + 1).toBe(2);
+  const onSubmit = vi.fn();
+  beforeEach(() => {
+    onSubmit.mockClear();
+    render(<Checkout onSubmit={onSubmit} />);
+  });
+
+  test('onSubmit() when all fields pass validation', async () => {
+    await user.type(getNameInput(), 'John Doe');
+    await user.type(getCardInput(), '1234 5678 9012 3456');
+    await user.type(getExpDateInput(), '12/34');
+    await user.type(getCvvInput(), '123');
+    await user.type(getZipCodeInput(), '12345');
+    clickConfirmButton();
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled();
+    });
+    expect(onSubmit).toHaveBeenCalledWith({
+      name: 'John Doe',
+      cardNumber: '1234 5678 9012 3456',
+      expDate: '12/34',
+      cvv: '123',
+      zipCode: '12345',
+    });
+  });
+  test('Every field has an error message when empty', async () => {
+    clickConfirmButton();
+    await waitFor(() => {
+      expect(getNameInput()).toHaveErrorMessage('Name cannot be empty');
+      expect(getCardInput()).toHaveErrorMessage(
+        'Card number must have 16 digits'
+      );
+      expect(getExpDateInput()).toHaveErrorMessage('Invalid expiration date');
+      expect(getCvvInput()).toHaveErrorMessage('Invalid CVV');
+      expect(getZipCodeInput()).toHaveErrorMessage('Zip Code cannot be empty');
+    });
   });
 });
+
+function getNameInput() {
+  return screen.getByRole('textbox', { name: /full name/i });
+}
+
+function getCardInput() {
+  return screen.getByRole('textbox', { name: /card number/i });
+}
+
+function getExpDateInput() {
+  return screen.getByRole('textbox', { name: /expiration/i });
+}
+
+function getCvvInput() {
+  return screen.getByRole('textbox', { name: /cvv/i });
+}
+
+function getZipCodeInput() {
+  return screen.getByRole('textbox', { name: /zip code/i });
+}
+
+function clickConfirmButton() {
+  user.click(screen.getByRole('button', { name: /confirm payment/i }));
+}
